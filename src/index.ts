82733b383536ae1ac33357a61dcf5166abfedb68
Daniel Foerster <pydsigner@gmail.com>
2022-06-14 20:10:39 -0500

    Update code to match changes to Hades

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata';
 
 import { HadesContainer } from 'hades';
-import { installTextCommands } from 'hades/dist/text-commands'
+import { defaultMappedTypes, TextCommandsInstaller } from 'hades/dist/text-commands'
 import { createConnection } from 'typeorm';
 
 import { BotService } from './services/BotService';
@@ -17,10 +17,11 @@ import './text-commands';
     });
 
     const container = new HadesContainer({
-        installers: [installTextCommands],
+        installers: [new TextCommandsInstaller(
+            [...defaultMappedTypes]
+        )]
     });
-
-    container.get(BotService);
+    const bot = container.get(BotService);
+    bot.login();
 
 })()).catch(e => { console.error(e); process.exit(1) });
-
